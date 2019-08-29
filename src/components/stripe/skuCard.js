17c1399d7ceeb458dcaa0889007503742c4f16ee
colpaerte <eli.colpaert@delaware.pro>
2019-08-29 11:45:36 +0200

    Add stripe images

--- src/components/stripe/skuCard.js
+++ src/components/stripe/skuCard.js
@@ -33,11 +33,11 @@ const SkuCard = class extends React.Component {
                 <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
                     <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                         <div className="flex flex-wrap no-underline hover:no-underline">
-                            <p className="w-full text-gray-600 text-xs md:text-sm px-6">{sku.product.name}</p>
-                            <div className="w-full font-bold text-xl text-gray-800 px-6">{sku.product.name}</div>
-                            <p className="text-gray-800 text-base px-6 mb-5">
-                                Price: {formatPrice(sku.price, sku.currency)}
+                            <p className="w-full text-gray-600 text-xs md:text-sm px-6">{formatPrice(sku.price, sku.currency)}
                             </p>
+                            <div className="w-full font-bold text-xl text-gray-800 px-6">{sku.product.name}</div>
+                            <img src={sku.image} className="h-40" alt={sku.product.name}></img>
+
                         </div>
                     </div>
                     <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
